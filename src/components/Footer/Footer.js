d64a2450474522273e072f4cf708171b3d5678fc
Jean Diouf <jdiouf@deloitte.com>
2021-02-21 22:28:26 -0500

    setting up core app components; added grid css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const footer = (props) => {
+    return (
+        <div className="footer footer-layout">
+            <footer className="row section">
+                <div className="row box">
+                    
+                </div>
+            </footer>
+        </div>
+    )
+}
+export default footer;
