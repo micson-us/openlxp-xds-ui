7a45720d94217355f6f16c712f1a1a0ce26946c6
Swathi Chittlur <schittlur@deloitte.com>
2021-03-01 09:35:09 -0500

    header background and end lines

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,28 +17,28 @@ const footer = (props) => {
                         <div className="row">
                             <h4 className={classes.text}>Links</h4>
                             <ul className="col span-1-of-3">
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/" target="_blank" className={classes.links}>Home</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/About-DoD-CIO/" target="_blank" className={classes.links}>About DOD CIO</a></li>
-                                <li className={classes.listLinks}><a href="https://www.defense.gov/" target="_blank" className={classes.links}>Defense.gov</a></li>
-                                <li className={classes.listLinks}><a href="https://open.defense.gov/Transparency/FOIA.aspx" target="_blank" className={classes.links}>FOIA</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/DoDSection508/Std_Stmt.aspx" target="_blank" className={classes.links}>Section 508</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Library/" target="_blank" className={classes.links}>Strategic Plans</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/" target="_blank" rel="noreferrer" className={classes.links}>Home</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/About-DoD-CIO/" target="_blank" rel="noreferrer" className={classes.links}>About DOD CIO</a></li>
+                                <li className={classes.listLinks}><a href="https://www.defense.gov/" target="_blank" rel="noreferrer" className={classes.links}>Defense.gov</a></li>
+                                <li className={classes.listLinks}><a href="https://open.defense.gov/Transparency/FOIA.aspx" target="_blank" rel="noreferrer" className={classes.links}>FOIA</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/DoDSection508/Std_Stmt.aspx" target="_blank" rel="noreferrer" className={classes.links}>Section 508</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Library/" target="_blank" rel="noreferrer" className={classes.links}>Strategic Plans</a></li>
                             </ul>
                             <ul className="col span-1-of-3">
-                                <li className={classes.listLinks}><a href="https://www.defense.gov//Resources/DOD-Information-Quality-Guidelines/" target="_blank" className={classes.links}>Information Quality</a></li>
-                                <li className={classes.listLinks}><a href="https://prhome.defense.gov/NoFear/" target="_blank" className={classes.links}>No FEAR Act</a></li>
-                                <li className={classes.listLinks}><a href="https://open.defense.gov/" target="_blank" className={classes.links}>Open Government</a></li>
-                                <li className={classes.listLinks}><a href="https://www.esd.whs.mil/DD/plainlanguage/" target="_blank" className={classes.links}>Plain Writing</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Home/Privacy-Policy.aspx" target="_blank" className={classes.links}>Privacy Policy</a></li>
-                                <li className={classes.listLinks}><a href="http://dpcld.defense.gov/Privacy/" target="_blank" className={classes.links}>Privacy Program</a></li>
+                                <li className={classes.listLinks}><a href="https://www.defense.gov//Resources/DOD-Information-Quality-Guidelines/" target="_blank" rel="noreferrer" className={classes.links}>Information Quality</a></li>
+                                <li className={classes.listLinks}><a href="https://prhome.defense.gov/NoFear/" target="_blank" rel="noreferrer" className={classes.links}>No FEAR Act</a></li>
+                                <li className={classes.listLinks}><a href="https://open.defense.gov/" target="_blank" rel="noreferrer" className={classes.links}>Open Government</a></li>
+                                <li className={classes.listLinks}><a href="https://www.esd.whs.mil/DD/plainlanguage/" target="_blank" rel="noreferrer" className={classes.links}>Plain Writing</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Home/Privacy-Policy.aspx" target="_blank" rel="noreferrer" className={classes.links}>Privacy Policy</a></li>
+                                <li className={classes.listLinks}><a href="http://dpcld.defense.gov/Privacy/" target="_blank" rel="noreferrer" className={classes.links}>Privacy Program</a></li>
                             </ul>
                             <ul className="col span-1-of-3">
-                                <li className={classes.listLinks}><a href="https://dod.usajobs.gov/" target="_blank" className={classes.links}>DoD Careers</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Home2/ELD.aspx" target="_blank" className={classes.links}>External Links Disclaimer</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Home/PublicUseNotice.aspx" target="_blank" className={classes.links}>Public Use Notice</a></li>
-                                <li className={classes.listLinks}><a href="hhttps://www.usa.gov/" target="_blank" className={classes.links}>USA.gov</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/DoD-Web-Policy/" target="_blank" className={classes.links}>Web Policy</a></li>
-                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Contact/" target="_blank" className={classes.links}>Contact US</a></li>
+                                <li className={classes.listLinks}><a href="https://dod.usajobs.gov/" target="_blank" rel="noreferrer" className={classes.links}>DoD Careers</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Home2/ELD.aspx" target="_blank" rel="noreferrer" className={classes.links}>External Links Disclaimer</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Home/PublicUseNotice.aspx" target="_blank" rel="noreferrer" className={classes.links}>Public Use Notice</a></li>
+                                <li className={classes.listLinks}><a href="hhttps://www.usa.gov/" target="_blank" rel="noreferrer" className={classes.links}>USA.gov</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/DoD-Web-Policy/" target="_blank" rel="noreferrer" className={classes.links}>Web Policy</a></li>
+                                <li className={classes.listLinks}><a href="https://dodcio.defense.gov/Contact/" target="_blank" rel="noreferrer" className={classes.links}>Contact US</a></li>
                             </ul> 
                         </div>
                     </section>
@@ -50,16 +50,16 @@ const footer = (props) => {
                             <h3 className={classes.text2}>STAY CONNECTED</h3>
                             <h3 className={classes.text2}>___________________________________________</h3>
                             <ul className="col span-1-of-3">
-                                <li className={classes.listLinks1}><a href="https://www.facebook.com/" target="_blank" className={classes.links}>Facebook</a></li>
-                                <li className={classes.listLinks1}><a href="https://twitter.com/Dod_cio" target="_blank" className={classes.links}>Twitter</a></li>
+                                <li className={classes.listLinks1}><a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className={classes.links}>Facebook</a></li>
+                                <li className={classes.listLinks1}><a href="https://twitter.com/Dod_cio" target="_blank" rel="noreferrer" className={classes.links}>Twitter</a></li>
                             </ul>
                             <ul className="col span-1-of-3">
-                                <li className={classes.listLinks1}><a href="https://www.youtube.com/" target="_blank" className={classes.links}>YouTube</a></li>
-                                <li className={classes.listLinks1}><a href="https://www.instagram.com/" target="_blank" className={classes.links}>Instagram</a></li>
+                                <li className={classes.listLinks1}><a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className={classes.links}>YouTube</a></li>
+                                <li className={classes.listLinks1}><a href="https://www.instagram.com/" target="_blank" rel="noreferrer" className={classes.links}>Instagram</a></li>
                             </ul>
                             <ul className="col span-1-of-3">
-                                <li className={classes.listLinks1}><a href="https://www.flickr.com/" target="_blank" className={classes.links}>Flickr</a></li>
-                                <li className={classes.listLinks1}><a href="https://www.defense.gov/Explore/Inside-DOD/" target="_blank" className={classes.links}>Blog</a></li>
+                                <li className={classes.listLinks1}><a href="https://www.flickr.com/" target="_blank" rel="noreferrer" className={classes.links}>Flickr</a></li>
+                                <li className={classes.listLinks1}><a href="https://www.defense.gov/Explore/Inside-DOD/" target="_blank" rel="noreferrer" className={classes.links}>Blog</a></li>
                             </ul> 
                         </div>
                     </section>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,7 @@ const Header = (props) => {
     });
 
     return (
+        <div className={classes.mainDiv} >
         <div className={classes.div} >
             <img className={classes.logo} src={logo} alt="logo" />
             <div className={classes.div1}>
@@ -19,22 +20,23 @@ const Header = (props) => {
                 <p> <b className={classes.title1}>U.S. Department of Defense</b> </p>
             </div>
             <div className={classes.div2} >
-            <form className={classes.search}>
-                {/* <input className={classes.text} type="text" placeholder= "Keyword search" value={this.state.value} onChange={this.handleChange} /> */}
-                <input className={classes.text} type="text" placeholder="Keyword search" value={state.keyword}
-                    onChange={event => {
-                        const newVal = event.target.value;
-                        setState(previousState => ({keyword: newVal}))
-                    }}/>
-                <input className={classes.searchImage} type="image" name="Submit" src={searchButton} alt="Submit" />
-                <Link className={classes.searchImage} type="image" name="Submit" src={searchButton} alt="Submit" to={{
-                        pathname: "/search/",
-                        search: "?q=" + state.keyword
-                    }}></Link>
-            </form>
-            
-            <NavLink to='/signIn' className={classes.signIn}>Sign in</NavLink>
+                <form className={classes.search}>
+                    {/* <input className={classes.text} type="text" placeholder= "Keyword search" value={this.state.value} onChange={this.handleChange} /> */}
+                    <input className={classes.text} type="text" placeholder="Keyword search" value={state.keyword}
+                        onChange={event => {
+                            const newVal = event.target.value;
+                            setState(previousState => ({keyword: newVal}))
+                        }}/>
+                    <input className={classes.searchImage} type="image" name="Submit" src={searchButton} alt="Submit" />
+                    <Link className={classes.searchImage} type="image" name="Submit" src={searchButton} alt="Submit" to={{
+                            pathname: "/search/",
+                            search: "?q=" + state.keyword
+                        }}></Link>
+                </form>
+                
+                <NavLink to='/signIn' className={classes.signIn}>Sign in</NavLink>
             </div>         
+        </div>
         </div>
     )
     
